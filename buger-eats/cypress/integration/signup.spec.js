726d8b8d72defdb0970e3f69066eb6655a9253e8
cskmacedo <kleber.barreto@msn.com>
2022-04-09 20:01:29 -0300

    Testes step by step

--- buger-eats/cypress/integration/signup.spec.js
+++ buger-eats/cypress/integration/signup.spec.js
@@ -63,15 +63,26 @@ describe('Cadastro', () => {
         signup.alertMessageShouldBe('Oops! Email com formato inválido.')
     })
 
-    it('Required fields', function() {
-        signupPage.go()
-        signupPage.submit()
-        signupPage.alertMessageShouldBe('É necessário informar o nome')
-        signupPage.alertMessageShouldBe('É necessário informar o CPF')
-        signupPage.alertMessageShouldBe('É necessário informar o email')
-        signupPage.alertMessageShouldBe('É necessário informar o CEP')
-        signupPage.alertMessageShouldBe('É necessário informar o número do endereço')
-        signupPage.alertMessageShouldBe('Selecione o método de entrega')
-        signupPage.alertMessageShouldBe('Adicione uma foto da sua CNH')
+    context('Required fields', function () {
+        const messages = [
+            {field: 'name', output: 'É necessário informar o nome'},
+            {field: 'cpf', output: 'É necessário informar o CPF'},
+            {field: 'email', output: 'É necessário informar o email'},
+            {field: 'postalcode', output: 'É necessário informar o CEP'},
+            {field: 'number', output: 'É necessário informar o número do endereço'},
+            {field: 'delivery_method', output: 'Selecione o método de entrega'},
+            {field: 'cnh', output: 'Adicione uma foto da sua CNH'}
+        ]
+
+        before(function() {
+            signupPage.go()
+            signupPage.submit()
+        })
+
+        messages.forEach(function (msg) {
+            it(`${msg.field} is required`, function () {
+                signupPage.alertMessageShouldBe(msg.output)
+            })
+        })
     })
 })
