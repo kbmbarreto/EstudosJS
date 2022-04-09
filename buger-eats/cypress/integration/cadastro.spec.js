05ae563d71f69adb52169bf13c9ae7970b7999b2
cskmacedo <kleber.barreto@msn.com>
2022-04-09 17:54:05 -0300

    Organização das massas de testes em fixtures

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -20,55 +20,25 @@ describe('Cadastro', () => {
     //     cy.log('Tudo aqui é executado sempre DEPOIS de cada caso de teste')
     // })
 
-    it('Usuário deve se tornar um entregador', () => {
+    beforeEach(function() {
+        cy.fixture('deliver').then((d) => {
+            this.deliver = d
+        })
+    })
 
-        //Massa de teste
-        var deliver = {
-            name: 'Kleber Barreto de Macedo',
-            cpf: '52959371003',
-            email: 'tikarakatika@zip.net',
-            whatsapp: '11999999999',
-            address: {
-                postalcode: '05564-200',
-                street: 'Avenida Engenheiro Heitor Antônio Eiras Garcia',
-                number: '627',
-                details: 'Apto 21',
-                district: 'Jardim Esmeralda',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'images/cnh-digital.jpg'
-        }
+    it('Usuário deve se tornar um entregador', function() {
 
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.signup)
         signup.submit()
         const expectedMessage = "Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato."
         signup.modalContentShouldBe(expectedMessage)
     })
 
-    it('CPF incorreto', () => {
-
-        //Massa de teste
-        var deliver = {
-            name: 'Kleber Barreto de Macedo',
-            cpf: '0000000000',
-            email: 'tikarakatika@zip.net',
-            whatsapp: '11999999999',
-            address: {
-                postalcode: '05564-200',
-                street: 'Avenida Engenheiro Heitor Antônio Eiras Garcia',
-                number: '627',
-                details: 'Apto 21',
-                district: 'Jardim Esmeralda',
-                city_state: 'São Paulo/SP'
-            },
-            delivery_method: 'Moto',
-            cnh: 'images/cnh-digital.jpg'
-        }
+    it('CPF incorreto', function() {
 
         signup.go()
-        signup.fillForm(deliver)
+        signup.fillForm(this.deliver.cpf_inv)
         signup.submit()
         signup.alertMessageShouldBe('Oops! CPF inválido')
     })
